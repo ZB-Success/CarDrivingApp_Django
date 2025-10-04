9ae0a241a4cf1c8fa2c0ce3550e8b59721e79701
ZBSuccess <bruhtesheme@gmail.com>
2025-10-04 17:50:55 +0300

    add configuration of the domain name

--- carDrivingApp/settings.py
+++ carDrivingApp/settings.py
@@ -28,6 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "car-driving-backend-djangoapp.onrender.com",
+    "https://car-driving-react-js-frontend.vercel.app/",
 ]
 
 # Application definition
